4ecbc657db338f3d3cfb5753de6b326fec0c0e11
Hamza Kanar <hamzakanar1139@outlook.com>
2024-05-30 16:47:06 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,23 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        // version 1 -- Çalışma Alanı - Working Space
+        System.out.println("hello GIT");
+        System.out.println("git add : staging areaya alır");
+
+        // cls terminal ekranını temizler
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
