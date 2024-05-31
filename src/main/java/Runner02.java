699e27da56b966a049967df6c8898b31056645ac
Hamza Kanar <hamzakanar1139@outlook.com>
2024-05-31 16:16:40 +0300

    version4

--- src/main/java/Runner02.java
+++ src/main/java/Runner02.java
@@ -1,7 +1,12 @@
 public class Runner02 {
     public static void main(String[] args) {
-        // versiyon 3
+        // version 3
         System.out.println("hello");
+
+        // version 4
+        System.out.println("SA");
+
+
 
 
 
